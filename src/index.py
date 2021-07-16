e95392f70edce2ef71d7543bd1bdec2bdb88f0d3
sebstrug <sebstrug@users.noreply.github.com>
2021-07-16 07:02:26 +0100

    Script to build index, tests

---
+++ src/index.py
@@ -0,0 +1,93 @@
+"""Index.py contais functions for creating an inverted index."""
+
+from collections import defaultdict
+from typing import Iterator
+import itertools
+from datetime import datetime
+import string
+
+import boto3
+
+from src import aws
+
+# Create a mapping from words to unique IDs
+# ~470,000 words in English, should be fine to store in memory
+WORD_ID_MAP = defaultdict(itertools.count().__next__)
+
+# Type alias for clarity
+WordFilePair = tuple[int, int]
+
+
+def get_text_word_ids(text: str) -> Iterator[int]:
+    """Get all the word IDs from a given text.
+    Punctuation is removed from the text, case is ignored.
+
+    Stopwords are not removed, this was not mentioned in the spec.
+
+    Args:
+        text: Some string to get word IDs for
+
+    Returns:
+        iterator of all word IDs in text
+    """
+    for word in text.split():
+        if word == "":
+            continue
+        # Remove punctuation, make string uniform
+        word = word.translate(str.maketrans(
+            "", "", string.punctuation)).lower()
+        yield WORD_ID_MAP[word]
+
+
+def get_word_file_pairs(file_id: int, text: str) -> Iterator[WordFilePair]:
+    """Given a file-id its text contents, return an iterator with file-id
+    and word-id pairs.
+
+    Args:
+        file_id: identifier for file
+        text: text contents of file
+
+    Returns:
+        iterator of (word-id, file-id) pairs
+    """
+    file_id_it = itertools.repeat(file_id)
+    word_id_it = get_text_word_ids(text)
+    return zip(word_id_it, file_id_it)
+
+
+def create_inverted_index(file_line_it: Iterator[str, str]) -> dict[int, list[str]]:
+    """Given an iterator producing pairs of (<file-id>, <line-from-file>), produce
+    an inverted index containing a mapping for each word ID to the set of all file IDs
+    that contain that word.
+
+    Args:
+        file_line_it: Iterator producing consecutive pairs of (<file-id>, <line-from-file>)
+
+    Returns:
+        mapping of word IDs to set of all file IDs that contain that word
+          i.e. <word-id>: <all-associated-file-ids>
+    """
+    word_file_map = defaultdict(list)
+    for fname, line in file_line_it:
+        word_file_it = get_word_file_pairs(fname, line)
+        for word_id, file_id in word_file_it:
+            word_file_map[word_id].append(file_id)
+
+    # Sorting once complete is faster than maintaining a sorted list with bisect
+    for word_id in word_file_map.keys():
+        word_file_map[word_id].sort()
+
+    return word_file_map
+
+
+def main():
+    s3 = boto3.resource("s3")
+
+    # Iterate through all lines in all S3 files, get (<word-id>,<file-id>) pairs
+    file_text_it = aws.iterate_text_pairs(s3, aws.BUCKET)
+    inverted_index = create_inverted_index(file_text_it)
+
+    dt_now = datetime.now().strftime("%Y-%m-%d--%H:%M")
+    aws.write_index(s3, aws.BUCKET, f"index--{dt_now}", inverted_index)
+    # Writing the word-id map is not mentioned in spec, but it's obviously required
+    aws.write_index(s3, aws.BUCKET, f"word-mapping--{dt_now}", WORD_ID_MAP)
---
+++ tests/test_index.py
@@ -0,0 +1,94 @@
+import unittest
+from collections import defaultdict
+import itertools
+
+from src import index
+
+
+class BasicTestCase(unittest.TestCase):
+    """Base test class to override the module level variable WORD_ID_MAP for each test"""
+
+    def setUp(self):
+        index.WORD_ID_MAP = defaultdict(itertools.count().__next__)
+
+
+class TestWordToId(BasicTestCase):
+    def test(self):
+        self.assertEqual(0, index.WORD_ID_MAP["foo"])
+        self.assertEqual(1, index.WORD_ID_MAP["bar"])
+        self.assertEqual(0, index.WORD_ID_MAP["foo"])
+        self.assertEqual(0, index.WORD_ID_MAP["foo"])
+        self.assertEqual(2, index.WORD_ID_MAP["baz"])
+
+
+class TestGetTextWordIds(BasicTestCase):
+    def test_empty(self):
+        it = index.get_text_word_ids("")
+        with self.assertRaises(StopIteration):
+            next(it)
+
+    def test(self):
+        it = index.get_text_word_ids("this is a line index this line")
+        for ind in range(0, 5):
+            self.assertEqual(ind, next(it))
+
+    def test_dirty(self):
+        it = index.get_text_word_ids('This is a line. Index this "line"')
+        for ind in range(0, 5):
+            self.assertEqual(ind, next(it))
+
+
+class TestGetWordFilePairs(BasicTestCase):
+    def test_empty(self):
+        it = index.get_word_file_pairs(1, "")
+        with self.assertRaises(StopIteration):
+            next(it)
+
+    def test(self):
+        it = index.get_word_file_pairs(1, "this is a line")
+        for ind in range(4):
+            self.assertEqual((ind, 1), next(it))
+
+    def test_multiline(self):
+        it = index.get_word_file_pairs(
+            2,
+            """this is a line
+this is another line""",
+        )
+        self.assertEqual((0, 2), next(it))
+        self.assertEqual((1, 2), next(it))
+        self.assertEqual((2, 2), next(it))
+        self.assertEqual((3, 2), next(it))
+        self.assertEqual((0, 2), next(it))
+        self.assertEqual((1, 2), next(it))
+        self.assertEqual((4, 2), next(it))
+        self.assertEqual((3, 2), next(it))
+
+
+class TestCreateInvertedIndex(BasicTestCase):
+    def test(self):
+        file_line_it = iter(
+            (
+                (4, "this is a file containing some lines"),
+                (1, "this is a file containing some lines"),
+                (2, "yet more lines here"),
+                (3, "here is another file"),
+            )
+        )
+
+        expected_output = {
+            0: [1, 4],
+            1: [1, 3, 4],
+            2: [1, 4],
+            3: [1, 3, 4],
+            4: [1, 4],
+            5: [1, 4],
+            6: [1, 2, 4],
+            7: [2],
+            8: [2],
+            9: [2, 3],
+            10: [3],
+        }
+        self.assertDictEqual(
+            expected_output, dict(index.create_inverted_index(file_line_it))
+        )
