85bf22bb70d0cecc69c166ac7faed82b5c5d60e2
sebstrug <sebstrug@users.noreply.github.com>
2021-07-25 19:54:24 +0100

    Use logging not print

--- src/api.py
+++ src/api.py
@@ -1,16 +1,17 @@
 from pathlib import Path
 from random import choice
+import logging
 
 from fastapi import FastAPI
 
-from src.local import LocalIndexHandler
+from src.handler import LocalIndexHandler
 
-local_path = Path('quotes')
+local_path = Path("quotes")
 index_handler = LocalIndexHandler(local_path)
-quotes_files = local_path.glob('*.txt')
+quotes_files = local_path.glob("*.txt")
 
-inverted_index = index_handler.load_index('index')
-word_id_map = index_handler.load_index('word-ids')
+inverted_index = index_handler.load_index("index")
+word_id_map = index_handler.load_index("word-ids")
 
 
 app = FastAPI()
@@ -24,16 +25,16 @@ def read_root():
 @app.get("/quotes/{word}")
 def find_quote(word: str):
     word_id = word_id_map.get(word)
-    print(f'Word: {word}')
-    print(f'Word ID: {word_id}')
+    logging.info(f"Word: {word}")
+    logging.info(f"Word ID: {word_id}")
     if not word_id:
-        return {'No': 'such quote'}
+        return {"quote": "N/A"}
     file_ids = inverted_index.get(str(word_id))
     file_id = choice(file_ids)
-    print(f'File IDs: {file_ids}\nFile ID: {file_id}')
+    logging.info(f"File IDs: {file_ids}\nFile ID: {file_id}")
 
-    quote_file = next(local_path.glob(f'{file_id}*.txt'))
-    with quote_file.open('r') as f:
+    quote_file = next(local_path.glob(f"{file_id}*.txt"))
+    with quote_file.open("r") as f:
         quote = f.read()
-    print(f'Quote: {quote}')
-    return {'quote': quote}
+    logging.info(f"Quote: {quote}")
+    return {"quote": quote}
