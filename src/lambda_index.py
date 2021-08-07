2499c57707c903adaa045c56db8ce3397a7b7e60
sebstrug <sebstrug@users.noreply.github.com>
2021-08-07 21:49:40 +0100

    Generate index using lambda function

---
+++ src/lambda_index.py
@@ -0,0 +1,19 @@
+"""Generate inverted index using AWS"""
+
+from typing import Dict
+
+import boto3
+
+from src.index import create_inverted_index, WORD_ID_MAP
+from src.handler import AWSHandler
+
+
+def lambda_handler(event: Dict[str, str], context):
+    s3 = boto3.resource("s3")
+    handler = AWSHandler(s3)
+
+    file_text_it = handler.iterate_text_pairs()
+    inverted_index = create_inverted_index(file_text_it)
+    handler.write_index("index", inverted_index)
+    handler.write_index("word-ids", WORD_ID_MAP)
+    return True
